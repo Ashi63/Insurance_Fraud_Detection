e0cb8ebfbb527f2d82a70032244992b0ea0d5495
Ashish <ashish.ashi6388@gmail.com>
2022-07-01 12:49:05 +0530

    Logger+rawValidation

---
+++ Training_Raw_Data_validation/rawValidation.py
@@ -0,0 +1,60 @@
+import json
+import sys
+import os
+import shutil
+sys.path.append("..")
+from application_logging.logger import App_Logger
+
+
+logger = App_Logger()
+
+def valuesfromSchema():
+    
+    path = "D:\Insurance Fraud Detection\\"
+    with open(path + "schema_training.json","r") as f:
+        dict = json.load(f)
+
+    pattern = dict['SampleFileName']
+    LengthOfDateStampInFile = dict['LengthOfDateStampInFile']
+    LengthOfTimeStampInFile = dict['LengthOfTimeStampInFile']
+    NumberofColumns = dict['NumberofColumns']
+    ColName = dict['ColName']
+
+    path_logs_dir = os.path.join("D:\Insurance Fraud Detection\\","Training_Logs")
+    path_log_file = os.path.join(path_logs_dir,"valuesfromSchemaValidationLog.txt")
+
+    file = open(path_log_file,'a+')
+    message = f"Length Of Date Stamp In File:: {LengthOfDateStampInFile}"+"\t\t"+f"Length Of Time Stamp In File:: {LengthOfTimeStampInFile}"+"\t\t"+ f"NumberofColumns:: {NumberofColumns}"+"\n"
+    logger.log(file,message)
+    file.close()
+
+
+def manualRegexCreation():
+    regex = "['fraudDetection']+['\_'']+[\d_]+[\d]+\.csv"
+    return regex
+
+def createDirectoryForGoodBadRawData():
+    try:
+        path = os.path.join("Training_Raw_files_validated/","Good_Raw/")
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        path = os.path.join("Training_Raw_files_validated/","Bad_Raw")
+        if not os.path.isdir(path):
+            os.makedirs(path)
+    except OSError as ex:
+        file = open ("Training_Logs/GeneralLogs.txt","a+")
+        logger.log(file,"Error in creating Directory")
+        file.close()
+        raise OSError
+
+
+valuesfromSchema()
+
+
+
+
+
+
+
+
+
---
+++ get_files.py
@@ -0,0 +1,7 @@
+import shutil
+
+src_path = r"D:\iNeuron\Machine Learning Projects iNeuron\6.Insurance Fraud Prediction\insuranceFraudDetection\insuranceFraudDetection\code\fraudDetection\schema_training.json"
+dest_path = r"D:\Insurance Fraud Detection\Insurance-Fraud-Detection"
+shutil.copy(src_path,dest_path)
+print("File Copied Succesfully")
+# TO copy files from one folder tot other
